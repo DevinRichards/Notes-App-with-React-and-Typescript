7621617c3ac06e0f6e976ac0677be775d58c1825
DevinRichards <devinmrichards95@gmail.com>
2024-07-08 21:59:18 -0700

    backend finished. working on updating frontend to link with backend.

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 type Note = {
@@ -8,38 +8,26 @@ type Note = {
 }
 
 const App = () => {
-  const [notes, setNotes] = useState<Note[]>([
-    {
-      id: 1,
-      title: "test note 1",
-      content: "bla bla note1",
-    },
-    {
-      id: 2,
-      title: "test note 2 ",
-      content: "bla bla note2",
-    },
-    {
-      id: 3,
-      title: "test note 3",
-      content: "bla bla note3",
-    },
-    {
-      id: 4,
-      title: "test note 4 ",
-      content: "bla bla note4",
-    },
-    {
-      id: 5,
-      title: "test note 5",
-      content: "bla bla note5",
-    },
-    {
-      id: 6,
-      title: "test note 6",
-      content: "bla bla note6",
-    },
-  ]);
+  const [notes, setNotes] = useState<Note[]>([])
+    
+
+    useEffect(() =>{
+      const fetchNotes = async () => {
+        try {
+          const response = await fetch(
+            "http://localhost:5000/notes"
+          );
+
+          const notes:Note[] =
+            await response.json();
+
+          setNotes(notes);
+        } catch (e) {
+          console.log(e);
+        }
+      };
+      fetchNotes();
+    }, []);
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -56,11 +44,14 @@ const App = () => {
     event: React.FormEvent
   ) => {
     event.preventDefault();
-    const newNote: Note = {
-      id: notes.length + 1,
-      title: title,
-      content: content
-    };
+    try{
+      const response = await fetch(
+        "http://localhost:5000/api/notes",
+        {
+          
+        }
+      )
+    }
 
     setNotes([newNote, ...notes]);
     setTitle("");
